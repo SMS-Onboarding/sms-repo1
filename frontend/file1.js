77d656b293213b8c2e171788553d722ec6493c10
Jack Moxon <jack@semgrep.com>
2024-09-12 10:54:56 -0700

    adding more files

---
+++ frontend/file1.js
@@ -0,0 +1,24 @@
+// Variables
+let greeting = "Hello";
+const name = "World";
+let count = 0;
+
+// Function to print a message
+function printMessage(message) {
+    console.log(message);
+}
+
+// Conditional to check the greeting
+if (greeting === "Hello") {
+    printMessage(`${greeting}, ${name}!`);
+} else {
+    printMessage("Goodbye!");
+}
+
+// Loop to count to 5
+for (let i = 1; i <= 5; i++) {
+    count++;
+    console.log(`Count is: ${count}`);
+}
+
+// A
